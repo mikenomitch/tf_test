c9e436926f75feccb66baf9c62e13ed00070abf0
Michael Nomitch <mike@Michaels-MacBook-Pro.local>
2019-04-10 14:27:32 -0500

    tweaks

--- main.tf
+++ main.tf
@@ -1,3 +1,30 @@
+// configure the github provider
+provider "github" {
+  organization = "mikenomitch"
+}
+
+// Configure the repository with the dynamically created Netlify key.
+resource "github_repository_deploy_key" "key" {
+  title      = "Netlify"
+  repository = "mikenomitch/tf_test"
+  key        = "${netlify_deploy_key.key.public_key}"
+  read_only  = false
+}
+
+// Create a webhook that triggers Netlify builds on push.
+resource "github_repository_webhook" "main" {
+  repository = "mikenomitch/tf_test"
+  name       = "web"
+  events     = ["delete", "push", "pull_request"]
+
+  configuration {
+    content_type = "json"
+    url          = "https://api.netlify.com/hooks/github"
+  }
+
+  depends_on = ["netlify_site.main"]
+}
+
 # Configure the Netlify Provider
 provider "netlify" {
   token = "${var.netlify_token}"
@@ -8,12 +35,13 @@ resource "netlify_deploy_key" "key" {}
 
 # Define your site
 resource "netlify_site" "main" {
-  name = "mikes-site"
+  name = "mike_tf_test_site"
 
   repo {
     deploy_key_id = "${netlify_deploy_key.key.id}"
     provider = "github"
     repo_path = "mikenomitch/tf_test"
     repo_branch = "master"
+    dir = "/"
   }
 }
