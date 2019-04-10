dd96e710d8cce6ec1c2c01e8d68de10cd9a07853
Michael Nomitch <mike@Michaels-MacBook-Pro.local>
2019-04-10 14:02:50 -0500

    Adding tf files

---
+++ main.tf
@@ -0,0 +1,19 @@
+# Configure the Netlify Provider
+provider "netlify" {
+  token = "${var.netlify_token}"
+}
+
+# Create a new deploy key for this specific website
+resource "netlify_deploy_key" "key" {}
+
+# Define your site
+resource "netlify_site" "main" {
+  name = "mikes-site"
+
+  repo {
+    deploy_key_id = "${netlify_deploy_key.key.id}"
+    provider = "github"
+    repo_path = "mnomitch/tf_test"
+    repo_branch = "master"
+  }
+}
