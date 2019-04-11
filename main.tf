9098bd10b6f43f747e272ccd45c5c89ac523753e
Michael Nomitch <mike@Michaels-MacBook-Pro.local>
2019-04-11 13:50:17 -0500

    another

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "netlify_deploy_key" "key" {}
 
 # Define your site
 resource "netlify_site" "main" {
-  name = "mike_tf_test_site_four_d"
+  name = "mike_tf_test_site_four_e"
 
   repo {
     deploy_key_id = "${netlify_deploy_key.key.id}"
